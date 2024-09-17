2a33dc4de3718e304ae65b2a6f5fafc1d046f80f
HungDoan <doananhlehung@gmail.com>
2024-09-18 02:37:31 +0700

    update

--- routes/web_admin.php
+++ routes/web_admin.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['locale','auth']], function(){
     Route::get('/overview-customer',  [DashboardController::class, 'customerOverview'])->name('overview.customer');
+    Route::get('/overview-partner',  [DashboardController::class, 'partnerOverview'])->name('overview.partner');
 });
