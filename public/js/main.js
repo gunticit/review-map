52721429a3f97f63dfcebe00c20e8d494eeeafe3
HungDoan <doananhlehung@gmail.com>
2024-10-17 23:48:38 +0700

    .

--- public/js/main.js
+++ public/js/main.js
@@ -45,33 +45,6 @@ $(document).ready(function () {
         $("#loading").fadeToggle(200);
     }
   
-});
-
-function generateCoupon(length) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let coupon = '';
-    
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        coupon += characters[randomIndex];
-    }
-    
-    return coupon;
-}  
-
-$(document).ready(function(){
-    $('#codeSelect').on('change', function () {
-        if($(this).val() == 1){
-            let code = generateCoupon(10);
-            $('#codeInput').val(code);
-            $('#codeInput').attr('readonly', 'readonly');
-            $('#codeInput').attr('disabled', 'disabled');
-        }else{
-            $('#codeInput').val('');
-            $('#codeInput').removeAttr('readonly');
-            $('#codeInput').removeAttr('disabled');
-        }
-    })
 
     function setupFileInput(inputFileSelector, fileListSelector, fileErrorSelector, maxSizeMB = 2) {
         let selectedFiles = []; // Array to store selected files
@@ -146,5 +119,32 @@ $(document).ready(function(){
             return selectedFiles; // Return the selected files array
         };
     }
+});
+
+function generateCoupon(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let coupon = '';
+    
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        coupon += characters[randomIndex];
+    }
+    
+    return coupon;
+}  
+
+$(document).ready(function(){
+    $('#codeSelect').on('change', function () {
+        if($(this).val() == 1){
+            let code = generateCoupon(10);
+            $('#codeInput').val(code);
+            $('#codeInput').attr('readonly', 'readonly');
+            $('#codeInput').attr('disabled', 'disabled');
+        }else{
+            $('#codeInput').val('');
+            $('#codeInput').removeAttr('readonly');
+            $('#codeInput').removeAttr('disabled');
+        }
+    })
     
 });
