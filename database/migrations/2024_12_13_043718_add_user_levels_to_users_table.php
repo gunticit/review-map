0d7d4a7d65c3b1437db4f9c39e198f18a8266687
HungDoan <doananhlehung@gmail.com>
2024-12-14 17:31:11 +0700

    update

--- database/migrations/2024_12_13_043718_add_user_levels_to_users_table.php
+++ database/migrations/2024_12_13_043718_add_user_levels_to_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['level', 'tasks_completed', 'experience_points']);
+            $table->dropColumn(['tasks_completed', 'experience_points']);
         });
     }
 };
