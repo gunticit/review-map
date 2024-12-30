ae6c7bb99108cda354ccba7237e801d17bdd4b41
Hung Doan <doananhlehung@gmail.com>
2024-12-30 13:48:52 +0700

    update time limit render comment

--- app/Services/CommentService.php
+++ app/Services/CommentService.php
@@ -94,60 +94,22 @@ class CommentService {
         }
         if(!empty($keywords)){
             $str_keyword = count($keywords) > 0 ? implode(', ', $keywords) : '';
-            if($sl_comment < 100){
-                $prompt = "Tạo cho tôi ".$sl_comment." bình luận, với mỗi bình luận cảm nhận ngắn không quá 120 ký tự với nội dung liên quan đến";
+            for($i = 1; $i <= ceil((int) $sl_comment / 25); $i++){
+                $prompt = "Tạo cho tôi 25 bình luận, với mỗi bình luận cảm nhận ngắn không quá 120 ký tự với nội dung liên quan đến";
                 if(!empty($description)){
                     $prompt .= " mô tả '".$description."' và";
                 }
                 if(!empty($str_keyword)){
                     $prompt .= " keyword chủ đề là: '(". $str_keyword .")'.";
                 }
-                $prompt .= " Yêu cầu bình luận tạo ra không đánh dấu số thứ tự và các bình luận được ngăn cách bởi ký tự | để phân biệt, và đủ số lượng bình luận là: ".$sl_comment." bình luận.";
+                $prompt .= " Yêu cầu bình luận tạo ra không đánh dấu số thứ tự và các bình luận được ngăn cách bởi ký tự | để phân biệt, và đủ số lượng bình luận là: 25 bình luận.";
                 if(!empty(Helper::getSetting('setting_ai_content'))){
                     $prompt .= ' Ngoài ra: '.Helper::getSetting('setting_ai_content');
                 }
                 $stream = Gemini::geminiPro()
                     ->generateContent($prompt);
                 if(!empty($stream->text())){
-                    $comments = $stream->text();
-                }
-            }else if($sl_comment == 100){
-                for($i = 1; $i <= 4; $i++){
-                    $prompt = "Tạo cho tôi 25 bình luận, với mỗi bình luận cảm nhận ngắn không quá 120 ký tự với nội dung liên quan đến";
-                    if(!empty($description)){
-                        $prompt .= " mô tả '".$description."' và";
-                    }
-                    if(!empty($str_keyword)){
-                        $prompt .= " keyword chủ đề là: '(". $str_keyword .")'.";
-                    }
-                    $prompt .= " Yêu cầu bình luận tạo ra không đánh dấu số thứ tự và các bình luận được ngăn cách bởi ký tự | để phân biệt, và đủ số lượng bình luận là: 25 bình luận.";
-                    if(!empty(Helper::getSetting('setting_ai_content'))){
-                        $prompt .= ' Ngoài ra: '.Helper::getSetting('setting_ai_content');
-                    }
-                    $stream = Gemini::geminiPro()
-                        ->generateContent($prompt);
-                    if(!empty($stream->text())){
-                        $comments .= $stream->text();
-                    }
-                }
-            }else if($sl_comment == 200){
-                for($i = 1; $i <= 8; $i++){
-                    $prompt = "Tạo cho tôi 25 bình luận, với mỗi bình luận cảm nhận ngắn không quá 120 ký tự với nội dung liên quan đến";
-                    if(!empty($description)){
-                        $prompt .= " mô tả '".$description."' và";
-                    }
-                    if(!empty($str_keyword)){
-                        $prompt .= " keyword chủ đề là: '(". $str_keyword .")'.";
-                    }
-                    $prompt .= " Yêu cầu bình luận tạo ra không đánh dấu số thứ tự và các bình luận được ngăn cách bởi ký tự | để phân biệt, và đủ số lượng bình luận là: 25 bình luận.";
-                    if(!empty(Helper::getSetting('setting_ai_content'))){
-                        $prompt .= ' Ngoài ra: '.Helper::getSetting('setting_ai_content');
-                    }
-                    $stream = Gemini::geminiPro()
-                        ->generateContent($prompt);
-                    if(!empty($stream->text())){
-                        $comments .= $stream->text();
-                    }
+                    $comments .= $stream->text();
                 }
             }
         }
